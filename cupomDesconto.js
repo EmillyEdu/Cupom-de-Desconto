c92ff47d7748252ee97765938bad7cd10e6decf4
EmillyEdu <85949433+EmillyEdu@users.noreply.github.com>
2021-06-26 18:10:29 -0300

    Update cupomDesconto.js

--- cupomDesconto.js
+++ cupomDesconto.js
@@ -1,17 +1,20 @@
-//cupom RANGOBARATO
-//12h as 13:59
-//10% de desconto
-// acima ou igual a 100 reais, 20% de desconto
+//Em uma loja , das 12h até as 13:59 com o cupom de desconto "rangobarato", tem um desconto de 10% no valor da compra abaixo de 100 reais e de 20% quando o valor da compra é maior que 100 reais
+//deseja-se descobrir o valor do desconto e o valor que deve ser pago pelo cliente.
+
 function valorDesconto (valorCompra, hora, cupom){
   let desconto;
-  if (cupom === "RANGOBARATO"){
-    if(hora >= 12 && hora<14){
+// se tiver o cupom
+   if (cupom === "RANGOBARATO"){
+//  se não estiver no horário que o cupomé válido     
+    if(hora >= 12 && hora<14){    
+// se o valor da compra for maior ou igual a 100 reais, o desconto é de 20%
       if(valorCompra >= 100){
         desconto=valorCompra*20/100
       }
       else{
         desconto=valorCompra*10/100
       }
+// mostrando qual é o desconto e qual o valor a ser pago
       console.log(`o valor do desconto é ${desconto}. Portanto, o valor a ser pago será ${valorCompra-desconto}`)
     }
     else {
